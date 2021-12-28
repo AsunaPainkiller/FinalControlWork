6f644af1538d106bdbfcf9cbba3766f14bfc3287
Rudenko Anastasiya <nzaveruha@list.ru>
2021-12-28 21:15:59 +0300

    added the first choise

--- Program.cs
+++ Program.cs
@@ -12,7 +12,32 @@ int choise = 0;
 Console.ReadLine(choise);
 if (choise == 1)
 {
-
+    Console.WriteLine("Введите количество элементов для массива, который вы хотите задать");
+    int i = 0;
+    Console.ReadLine(i);
+    int[] arr = new int[i];
+    int count = 0;
+    Console.WriteLine("Построчно, через Enter, введите пожалуйста каждый элемент массива");
+    while (count <i)
+    {
+        arr[count] = Convert.ToInt32(Console.ReadLine());
+        count++;
+    }
+    for (count = 0; count <i; count++)
+    {
+        if (arr[count]%2 == 0)
+        {
+            fin[count] = arr[i];
+        }
+        int size = sizeof(fin);
+    }
+    count = 0;
+    Console.WriteLine($"Конечный результат:");
+    while (count <size)
+    {
+        Console.WriteLine($"{fin[count]}");
+        count++;
+    }
 }
 if (choise == 2)
 {
